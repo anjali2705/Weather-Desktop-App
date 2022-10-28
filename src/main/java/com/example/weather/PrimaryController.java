969b0c8978e3af2d72867b60921d8d15fb0b5e8c
anjali2705 <anjali27051997@gmail.com>
2022-10-28 13:35:59 +0530

    Weather Desktop App Using Java

---
+++ src/main/java/com/example/weather/PrimaryController.java
@@ -0,0 +1,50 @@
+package com.example.weather;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.net.MalformedURLException;
+import java.util.Map;
+
+public class PrimaryController {
+
+    @FXML
+    private TextField cityInput;
+
+    @FXML
+    private Text weatherText;
+
+    private final String cityAPI = "http://api.openweathermap.org/data/2.5/weather?appid=730699c5933b5c9448d0112c148d1fae&units=metric&q=";
+
+
+    @FXML
+    void getWeatherData(ActionEvent event) throws MalformedURLException {
+        JSONObject todaysWeather =getWoeid();
+
+        JSONObject main = (JSONObject) todaysWeather.get("main");
+
+        System.out.println(main);
+
+        weatherText.setText(
+                "Min temperature: " + main.get("temp_min") + " Celsius" +
+                        "\nCurrent temperature: " + main.get("temp") + " Celsius" +
+                        "\nMax temperature: " + main.get("temp_max") +" Celsius"
+        );
+    }
+
+    public JSONObject getWoeid() throws MalformedURLException {
+        APIConnector apiConnectorCity = new APIConnector(cityAPI);
+
+          JSONObject jsonData =  (apiConnectorCity.getJSONObject(cityInput.getText()));
+
+        System.out.println(jsonData);
+        return jsonData;
+    }
+
+
+}
